33d17fc847e8cb1e1a39c78487b803cf3c043e9a
VIRG <bazzoink1702@gmail.com>
2015-11-05 21:14:15 +0800

    tooltip-template shindig

--- app/scripts/directives/tooltipTemplate.js
+++ app/scripts/directives/tooltipTemplate.js
@@ -2,46 +2,96 @@
   "use strict";
 
   angular.module("testApp")
-
     .directive("tooltipTemplate", [
       "$templateCache",
       "$compile",
-      function($templateCache, $compile) {
+      "$timeout",
+      "$parse",
+      function($templateCache, $compile, $timeout, $parse) {
         return {
           restrict: "A",
           replace: false,
           terminal: true,
           priority: 1000,
-          link: function(scope, element, attrs) {
+          link: function link(scope, element, attrs) {
             if (!attrs.tooltipTemplate) {
               return;
             }
-            var template = $templateCache.get(attrs.tooltipTemplate);
-            var templateEl = $compile(template)(scope);
-
-            var result = $("<div></div>").append(templateEl);
-
             // http://stackoverflow.com/questions/19224028/add-directives-from-directive-in-angularjs
-
             element.removeAttr("tooltip-template");
             element.removeAttr("data-tooltip-template");
 
-            element.attr("tooltip-html-unsafe", result.html());
-            $compile(element)(scope);
+            var childScope = scope.$new();
+            var template = $templateCache.get(attrs.tooltipTemplate);
+            var linkedTemplate = $compile(template)(childScope);
+
+            function setTooltip() {
+              var linkedEl = $("<div></div>").append(linkedTemplate);
+              element.attr("tooltip-html-unsafe", linkedEl.html());
+
+              $compile(element)(scope);
+            }
+
+            if (attrs.tooltipTemplateRefresh) {
+              var compileTimeout;
+
+              childScope.$watchCollection(
+                function() {
+                  var collection = [];
+                  var keys = $parse(attrs.tooltipTemplateRefresh)(scope);
+                  for (var i = 0; i < keys.length; i++) {
+                    collection.push($parse(keys[i])(childScope));
+                  }
+                  return collection;
+                },
+                function(newVal, oldVal){
+                  console.info(newVal, oldVal)
+                  if (compileTimeout) {
+                    $timeout.cancel(compileTimeout);
+                  }
+                  compileTimeout = $timeout(function(){
+                    setTooltip();
+                  }, 100);
+                }
+              );
+            } else {
+              setTooltip();
+            }
+
           }
         };
       }
     ])
 
     .directive("testDirective", [
-      function() {
+      "$q",
+      function($q) {
         return {
           restrict: "A",
-          template: "<strong>{{waitForResult}}</strong>",
-          link: function(scope) {
+          controller: function(){
+            var self = this;
+
+            this.setResult = function(result){
+              self.result = result;
+            };
+
+            this.clearResult = function(){
+              self.result = "CLEARED!";
+            };
+          },
+          controllerAs: "testCtrl",
+          scope: false,
+          template: "<h1>TEST</h1><strong>{{testCtrl.result}}</strong><button class='ng-hide' ng-show='!!testCtrl.result' ng-click='testCtrl.clearResult()'>CLEAR RESULT</button>",
+          link: function(scope, element, attrs, ctrl) {
+            var deferred = $q.defer();
             setTimeout(function(){
-              scope.waitForResult = "I AM UPDATED!!!";
+              deferred.resolve("I AM UPDATED!!!");
             }, 5000);
+
+            deferred.promise.then(function(result){
+              console.info("promise resolved", result);
+              ctrl.setResult(result);
+            });
           }
         };
       }
